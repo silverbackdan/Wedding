897826d6e163f8b14d33bc6421d21370c14bba57
Daniel West <daniel@silverback.is>
2018-04-09 10:51:27 +0100

    Composer deps update

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -6,4 +6,6 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
